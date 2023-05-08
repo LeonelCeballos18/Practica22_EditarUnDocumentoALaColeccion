616edabda6ecebb69fabaf26867f13f180eaaa4a
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2023-05-07 19:25:38 -0600

    Avance

--- src/routes/updateUser.js
+++ src/routes/updateUser.js
@@ -3,9 +3,10 @@ let router = express.Router(); //Instalacion del router
 const mongoose = require('mongoose'); //Inyectamos la dependencia de mongoose
 let User = require('../models/users'); //Le indicamos la direccion de nuestro modelo
 
-router.get('/users', async (req, res)=>{
-    const Users = await User.find({}); //Recuperamos los datos de nuestro schema
-    res.render("index", {Users}); //Imprimimos lo que recuperamos en una vista
+router.get('/findById/:id', (req, res)=>{
+    User.findById(req.params.id)
+    .then((myUser)=>{res.render('updateUser', {myUser})})
+    .catch((error)=>{res.json({message:error})})
 })
 
 module.exports = router;
